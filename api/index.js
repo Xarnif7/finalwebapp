59f81ec757385457e1f2c721047aadcec10e3862
Xarnif7 <shirley.xane@gmail.com>
2025-09-21 00:01:42 -0600

    Fix static asset serving and update page title to 'Blipp'

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ export default function handler(req, res) {
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/favicon-16.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Blipp - Review Automation for Home Service Businesses</title>
+    <title>Blipp</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
