11f238197773dbb272ded6341ca957546729fb64
Xarnif7 <shirley.xane@gmail.com>
2025-09-25 01:52:30 -0600

    fix duplicate PORT declaration causing server startup failure

--- server.js
+++ server.js
@@ -10928,7 +10928,6 @@ app.get('/api/reviews', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
